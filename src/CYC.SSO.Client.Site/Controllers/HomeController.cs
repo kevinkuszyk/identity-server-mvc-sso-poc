3c70a361731c9644f6a52bb38eca5dd7ce4d250f
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-09-25 14:36:17 +0100

    Add open id connect to client site

--- src/CYC.SSO.Client.Site/Controllers/HomeController.cs
+++ src/CYC.SSO.Client.Site/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,11 +14,10 @@ namespace CYC.SSO.Client.Site.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return View((User as ClaimsPrincipal).Claims);
         }
 
         public ActionResult Contact()
---
+++ src/CYC.SSO.Client.Site/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+
+namespace CYC.SSO.Client.Site
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies"
+            });
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                Authority = "http://localhost:33841/identity",
+                ClientId = "mvc",
+                RedirectUri = "http://localhost:33881/",
+                ResponseType = "id_token",
+                
+                SignInAsAuthenticationType = "Cookies"
+            });
+
+        }
+
+        
+    }
+}
